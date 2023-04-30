5998b33c70b10d3bdcd15a0531bd46a21fafbc82
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-04-30 19:14:40 +0300

    minor changes and refactoring Deprecated MaxWidth

--- SemanticXamlPrint/DefaultXamlGraphics.cs
+++ SemanticXamlPrint/DefaultXamlGraphics.cs
@@ -6,20 +6,17 @@ namespace SemanticXamlPrint
 {
     public static class DefaultXamlGraphics
     {
-        public static void DrawXamlComponent(this Graphics graphics, IXamlComponent xamlComponent)
+        public static float DrawXamlComponent(this Graphics graphics, IXamlComponent xamlComponent, float yPositionDraw = 0)
         {
-            if (xamlComponent == null) return;
+            if (xamlComponent == null) return yPositionDraw;
             if (xamlComponent.Type != typeof(TemplateComponent)) throw new Exception($"Root Component must be that of a [{nameof(TemplateComponent)}] but currently is: [{xamlComponent.Name}]");
             TemplateComponent Template = (TemplateComponent)xamlComponent;
             ComponentDrawingFormatting TemplateFormatting = Template.GetSystemDrawingProperties(Defaults.Formatting) ?? throw new Exception("Default template properties are missing");
-            if (Template.MaxWidth <= 50) Template.MaxWidth = 290;
-            if (Template.MarginTop < 0) Template.MarginTop = 20;
-            float _currentLineY = Template.MarginTop;
+            float _currentLineY = yPositionDraw + Template.MarginTop;
             //Draw Root Component Children
             for (int i = 0; i < Template?.Children?.Count; i++)
-            {
                 _currentLineY = graphics.DrawComponent(Template?.Children[i], TemplateFormatting, 0, _currentLineY, graphics.VisibleClipBounds.Width);
-            }
+            return _currentLineY;
         }
     }
 }

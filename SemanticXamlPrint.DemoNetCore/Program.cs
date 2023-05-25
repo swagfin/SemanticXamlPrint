fcb9a71e70556ffc91bae2bcb397607a8ca91cc0
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-05-26 01:13:06 +0300

    PDF Net core updates NOTE QR CODE NOT SUPPORTED FOR NOW ON .NET CORE

---
+++ SemanticXamlPrint.DemoNetCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+using SemanticXamlPrint.Parser;
+using SemanticXamlPrint.Parser.Components;
+using SemanticXamlPrint.PDF.NetCore;
+using System;
+using System.IO;
+namespace SemanticXamlPrint.DemoNetCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TESTING XAML PRINT");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            try
+            {
+                //Get Template Contents
+                byte[] xamlFileBytes = File.ReadAllBytes("custom.grid.template");
+                //Use Default Parser 
+                IXamlComponent xamlComponent = DefaultXamlParser.Parse(xamlFileBytes);
+                //####  PDF SHARP #####
+                using (PdfDocument document = new PdfDocument())
+                {
+                    // Add a page to the document
+                    PdfPage page = document.AddPage();
+                    // Create a graphics object for the page
+                    XGraphics xgraphics = XGraphics.FromPdfPage(page);
+                    //Use Xaml Draw Extension to Generate PDF
+                    xgraphics.DrawXamlComponent(xamlComponent);
+                    // Save the PDF document to a file
+                    document.Save("outputcore.pdf");
+                }
+                //####  PDF SHARP #####
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message.ToString());
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- SemanticXamlPrint.PDF.NetCore/Extensions/XGraphicsQRCodeExtensions.cs
+++ SemanticXamlPrint.PDF.NetCore/Extensions/XGraphicsQRCodeExtensions.cs
@@ -1,8 +1,4 @@
 ﻿using PdfSharpCore.Drawing;
-using System;
-using ZXing;
-using ZXing.QrCode;
-using ZXing.QrCode.Internal;
 
 namespace SemanticXamlPrint.PDF.NetCore
 {
@@ -10,23 +6,7 @@ namespace SemanticXamlPrint.PDF.NetCore
     {
         public static int DrawQRCodeCenteredAndReturnHeight(this XGraphics graphics, string text, float x, float y, float maxWidth, float maxHeight, float maxLayoutWith)
         {
-            // Generate QR code image using ZXing.Net
-            BarcodeWriter<XBitmapImage> writer = new BarcodeWriter<XBitmapImage>
-            {
-                Format = BarcodeFormat.QR_CODE,
-                Options = new QrCodeEncodingOptions
-                {
-                    ErrorCorrection = ErrorCorrectionLevel.Q,
-                    Width = 10,
-                    Height = 10
-                }
-            };
-            XBitmapImage qrCodeImage = writer.Write(text);
-            float newWidth = Math.Min(qrCodeImage.PixelWidth, maxWidth > 0 ? maxWidth : qrCodeImage.PixelWidth);
-            float newHeight = Math.Min(qrCodeImage.PixelHeight, maxHeight > 0 ? maxHeight : qrCodeImage.PixelHeight);
-            float centeredX = x + (maxLayoutWith - newWidth) / 2;
-            graphics.DrawImage(qrCodeImage, centeredX > 0 ? centeredX : x, y, newWidth, newHeight);
-            return (int)newHeight;
+            return graphics.DrawStringAndReturnHeight("QR CODE is not supported on .NET Core", true, XDefaults.Formatting, x, y, maxLayoutWith);
         }
     }
 }

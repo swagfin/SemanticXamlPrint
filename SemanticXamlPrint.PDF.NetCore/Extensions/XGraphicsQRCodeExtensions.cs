515a9273f78b47bc7d2c31694ab6580ec186ec92
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-05-26 00:42:24 +0300

    chore: added .net core support lib

---
+++ SemanticXamlPrint.PDF.NetCore/Extensions/XGraphicsQRCodeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using PdfSharpCore.Drawing;
+using System;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace SemanticXamlPrint.PDF.NetCore
+{
+    internal static class XGraphicsQRCodeExtensions
+    {
+        public static int DrawQRCodeCenteredAndReturnHeight(this XGraphics graphics, string text, float x, float y, float maxWidth, float maxHeight, float maxLayoutWith)
+        {
+            // Generate QR code image using ZXing.Net
+            BarcodeWriter<XBitmapImage> writer = new BarcodeWriter<XBitmapImage>
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    ErrorCorrection = ErrorCorrectionLevel.Q,
+                    Width = 10,
+                    Height = 10
+                }
+            };
+            XBitmapImage qrCodeImage = writer.Write(text);
+            float newWidth = Math.Min(qrCodeImage.PixelWidth, maxWidth > 0 ? maxWidth : qrCodeImage.PixelWidth);
+            float newHeight = Math.Min(qrCodeImage.PixelHeight, maxHeight > 0 ? maxHeight : qrCodeImage.PixelHeight);
+            float centeredX = x + (maxLayoutWith - newWidth) / 2;
+            graphics.DrawImage(qrCodeImage, centeredX > 0 ? centeredX : x, y, newWidth, newHeight);
+            return (int)newHeight;
+        }
+    }
+}
---
+++ SemanticXamlPrint.PDF.NetCore/_XDefaults.cs
@@ -0,0 +1,14 @@
+﻿using PdfSharpCore.Drawing;
+
+namespace SemanticXamlPrint.PDF.NetCore
+{
+    public static class XDefaults
+    {
+        public static ComponentXDrawingFormatting Formatting = new ComponentXDrawingFormatting
+        {
+            Font = new XFont("Calibri", 12, XFontStyle.Regular),
+            StringFormat = new XStringFormat { Alignment = XStringAlignment.Near, LineAlignment = XLineAlignment.Near },
+            Brush = XBrushes.Black
+        };
+    }
+}

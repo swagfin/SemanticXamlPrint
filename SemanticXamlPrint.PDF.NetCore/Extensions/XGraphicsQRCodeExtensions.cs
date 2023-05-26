ab50d85ba3294b4f53b2447fdf22a852eb043ce6
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-05-26 17:59:21 +0300

    added support for QR Code Generation

--- SemanticXamlPrint.PDF.NetCore/Extensions/XGraphicsQRCodeExtensions.cs
+++ SemanticXamlPrint.PDF.NetCore/Extensions/XGraphicsQRCodeExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using PdfSharpCore.Drawing;
+using QRCoder;
+using System;
+using System.IO;
 
 namespace SemanticXamlPrint.PDF.NetCore
 {
@@ -6,7 +9,23 @@ namespace SemanticXamlPrint.PDF.NetCore
     {
         public static int DrawQRCodeCenteredAndReturnHeight(this XGraphics graphics, string text, float x, float y, float maxWidth, float maxHeight, float maxLayoutWith)
         {
-            return graphics.DrawStringAndReturnHeight("QR CODE is not supported on .NET Core", true, XDefaults.Formatting, x, y, maxLayoutWith);
+            //Generate QR Code
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(text ?? "unspecified", QRCodeGenerator.ECCLevel.Q))
+            using (QRCode qrCode = new QRCode(qrCodeData))
+            {
+                using (MemoryStream stream = new MemoryStream())
+                using (SixLabors.ImageSharp.Image qrCodeImage = qrCode.GetGraphic(10))
+                {
+                    qrCodeImage.Save(stream, new SixLabors.ImageSharp.Formats.Png.PngEncoder());
+                    byte[] streamBytes = stream.ToArray();
+                    float newWidth = Math.Min(qrCodeImage.Height, maxWidth > 0 ? maxWidth : qrCodeImage.Width);
+                    float newHeight = Math.Min(qrCodeImage.Height, maxHeight > 0 ? maxHeight : qrCodeImage.Height);
+                    float centeredX = x + (maxLayoutWith - newWidth) / 2;
+                    graphics.DrawImage(XImage.FromStream(() => { return new MemoryStream(streamBytes); }), centeredX > 0 ? centeredX : x, y, newWidth, newHeight);
+                    return (int)newHeight;
+                }
+            }
         }
     }
 }

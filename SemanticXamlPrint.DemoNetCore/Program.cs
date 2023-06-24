08dda1f23e7b5b6285e84d92e49bc55f174f02b6
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-06-24 16:46:35 +0300

    chore: adding core logic

--- SemanticXamlPrint.DemoNetCore/Program.cs
+++ SemanticXamlPrint.DemoNetCore/Program.cs
@@ -1,10 +1,10 @@
-﻿using PdfSharpCore.Drawing;
-using PdfSharpCore.Pdf;
+﻿using PdfSharpCore.Pdf;
 using SemanticXamlPrint.Parser;
 using SemanticXamlPrint.Parser.Components;
 using SemanticXamlPrint.PDF.NetCore;
 using System;
 using System.IO;
+
 namespace SemanticXamlPrint.DemoNetCore
 {
     internal class Program
@@ -17,22 +17,50 @@ namespace SemanticXamlPrint.DemoNetCore
             try
             {
                 //Get Template Contents
-                byte[] xamlFileBytes = File.ReadAllBytes("custom.grid.template");
+                byte[] xamlFileBytes = File.ReadAllBytes("custom.excessgrid.template");
                 //Use Default Parser 
                 IXamlComponent xamlComponent = DefaultXamlParser.Parse(xamlFileBytes);
+
+
+                ////####  PDF SHARP #####
+                //using (PdfDocument document = new PdfDocument())
+                //{
+                //    // Add a page to the document
+                //    PdfPage page = document.AddPage();
+                //    // Create a graphics object for the page
+                //    XGraphics xgraphics = XGraphics.FromPdfPage(page);
+                //    //Use Xaml Draw Extension to Generate PDF
+                //    xgraphics.DrawXamlComponent(xamlComponent);
+                //    // Save the PDF document to a file
+                //    document.Save("outputcore.pdf");
+                //}
+                ////####  PDF SHARP #####
+
+                //              var stringBuilder = new StringBuilder();
+                //              for (int i = 15; i < 100; i++)
+                //              {
+                //                  string template = @$"	<GridRow>
+                //	<Data Grid.Column=""0"" Align=""Center"">{i}</Data>
+                //	<Data Grid.Column=""1"" TextWrap=""True"">Product {i}</Data>
+                //	<Data Grid.Column=""2"" Align=""Right"">{100 + i}.00</Data>
+                //</GridRow>";
+
+                //                  stringBuilder.Append(template);
+                //              }
+
+                //              string contents = stringBuilder.ToString();
+
                 //####  PDF SHARP #####
                 using (PdfDocument document = new PdfDocument())
                 {
-                    // Add a page to the document
-                    PdfPage page = document.AddPage();
-                    // Create a graphics object for the page
-                    XGraphics xgraphics = XGraphics.FromPdfPage(page);
                     //Use Xaml Draw Extension to Generate PDF
-                    xgraphics.DrawXamlComponent(xamlComponent);
+                    document.DrawXamlComponent(xamlComponent);
                     // Save the PDF document to a file
                     document.Save("outputcore.pdf");
                 }
                 //####  PDF SHARP #####
+
+
             }
             catch (Exception ex)
             {

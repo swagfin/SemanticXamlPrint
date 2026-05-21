6ae7d7676193da20470006437539bdf7f920995e
George Njeri (Swagfin) <georgewainaina18@gmail.com>
2026-05-21 23:14:43 +0300

    chore: demo item

--- SemanticXamlPrint.Demo/Program.cs
+++ SemanticXamlPrint.Demo/Program.cs
@@ -17,7 +17,7 @@ namespace SemanticXamlPrint.Demo
             try
             {
                 //Get Template Contents
-                byte[] xamlFileBytes = File.ReadAllBytes("custom.excessgrid.template");
+                byte[] xamlFileBytes = File.ReadAllBytes("custom.A4.template");
                 //Use Default Parser 
                 IXamlComponent xamlComponent = DefaultXamlParser.Parse(xamlFileBytes);
 
